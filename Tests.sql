59cde2562b4d26b2d02a11efb9f1f5683733e91d
Ting Zhu <judyustc@gmail.com>
2021-10-01 01:47:35 -0400

    Create Tests.sql

---
+++ Tests.sql
@@ -0,0 +1,16 @@
+/* Top 5 pieces of content */
+select Content_id
+from Content_Metadata
+order by version DESC
+limit 5;
+
+/* Aggregations across a rolling 30,60,90 days */
+
+
+/* Averate user session duration trends */
+
+/* Follow user Journeys for registration conversion */
+
+/* Generate user segments based on content */
+
+/* Generate dashboards to represent registered vs non-registered users */
