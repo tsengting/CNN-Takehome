d71728d96ed5febd9a721171e866a4c4dbd8cc49
Ting Zhu <judyustc@gmail.com>
2021-10-01 12:05:03 -0400

    Update Data Model.sql

--- Data Model.sql
+++ Data Model.sql
@@ -1,23 +1,22 @@
-create type varchararray as varchar(255) array[varchar(255)];
+CREATE TYPE varchararray AS varchar(255) array[varchar(255)];
 
-
-create type objtyp as object
+CREATE TYPE objtyp AS object
 (
    c1    integer,
    c2    varchar(255)
 );
 
-create table Content_Metadata (Content_Id varchar(255), Content_Type varchar(255), Creation_Date datetime, Version integer, Body varchararray, 
+CREATE TABLE Content_Metadata (Content_Id varchar(255), Content_Type varchar(255), Creation_Date datetime, Version integer, Body varchararray, 
                                Author varchar(255), Related_Media varchararray);
 
-create table Page_Impression (Content_Id varchar(255), Ads_User_Id varchar(255), Page_URL varchar(255), Session_Id varchar(255), IP_Address varchar(255),
+CREATE TABLE Page_Impression (Content_Id varchar(255), Ads_User_Id varchar(255), Page_URL varchar(255), Session_Id varchar(255), IP_Address varchar(255),
                               TimeStamp datetime, Referrer_URL varchar(255));
 
-create table Ad_Service_Interaction_Data (Ads_User_Id varchar(255), Ad_Id varchar(255), Ad_X_Position integer, Ad_Y_Position integer, Dwell_Time float, 
+CREATE TABLE Ad_Service_Interaction_Data (Ads_User_Id varchar(255), Ad_Id varchar(255), Ad_X_Position integer, Ad_Y_Position integer, Dwell_Time float, 
                                           Timestamp datetime);
 
-create table OAuth_Id_Service (OAuth_Id varchar(255), Ads_User_Id varchar(255), OAuth_Provider integer, Timestamp datetime);
+CREATE TABLE OAuth_Id_Service (OAuth_Id varchar(255), Ads_User_Id varchar(255), OAuth_Provider integer, Timestamp datetime);
 
-create table Registered_Users (User_Id varchar(255), OAuth_Id varchar(255), Display_Name integer, Preferences objtyp, Status varchar(255), Register_Date date,
+CREATE TABLE Registered_Users (User_Id varchar(255), OAuth_Id varchar(255), Display_Name integer, Preferences objtyp, Status varchar(255), Register_Date date,
                               Last_Access_time datetime);
 
